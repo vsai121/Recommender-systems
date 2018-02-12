1f6459f7f61de0147a8d9e18b53e0976c5d8da3e
vsai121 <vsaicharan1998@gmail.com>
2018-02-13 00:37:25 +0530

    First commit

---
+++ movilens.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+#Reading users file:
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,encoding='latin-1')
+
+#Reading ratings file:
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+data = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,encoding='latin-1')
+
+#Reading items file:
+i_cols = ['user_id' , 'movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+ 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+ 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,encoding='latin-1')
+
+"""
+print(users.head())
+print(users.shape)
+
+print(data.head())
+print(data.shape)
+
+print(items.head())
+print(items.shape)
+
+
+print(users.info())
+print(data.info())
+print(items.info())
+
+"""
+
+"""
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+
+"""
+
+
+nusers = users.shape[0]
+nitems = items.shape[0]
+
+"""
+print(nusers)
+print(nitems)
+
+"""
+
+#To create the user-item matrix
+
+ratings = np.zeros((nusers, nitems))
+for row in data.itertuples():
+    ratings[row[1]-1, row[2]-1] = row[3]
+
+"""
+print(ratings)
+
+"""
+
+#To calculate sparsity of the user-item matrix
+"""
+sparsity = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0] * ratings.shape[1])
+sparsity *= 100
+print 'Sparsity: {:4.2f}%'.format(sparsity)
+"""
+
+
+def train_test_split(ratings):
+    test = np.zeros(ratings.shape)
+    train = ratings.copy()
+    for user in xrange(ratings.shape[0]):
+        test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
+                                        size=10, 
+                                        replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings] = ratings[user, test_ratings]
+        
+    # Test and training are truly disjoint
+    assert(np.all((train * test) == 0)) 
+    return train, test
+
+train,test = train_test_split(ratings)
+"""
+print(train.shape)
+print(test.shape)
+"""
+
+#Similarity between users by using cosine distance
+def fast_similarity(ratings, kind='user', epsilon=1e-9):
+    # epsilon -> small number for handling dived-by-zero errors
+    if kind == 'user':
+        sim = ratings.dot(ratings.T) + epsilon
+    elif kind == 'item':
+        sim = ratings.T.dot(ratings) + epsilon
+    norms = np.array([np.sqrt(np.diagonal(sim))])
+    return (sim / norms / norms.T)
+
+
+user_similarity = fast_similarity(train, kind='user')
+print(user_similarity.shape)
+
+item_similarity = fast_similarity(train , kind='item')
+print(item_similarity.shape)
+
+
+def predict_fast_simple(ratings, similarity, kind='user'):
+    if kind == 'user':
+        return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif kind == 'item':
+        return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+
+def get_mse(pred, actual):
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+
+user_prediction = predict_fast_simple(train, user_similarity, kind='user')
+item_prediction = predict_fast_simple(train , item_similarity, kind='item')
+
+print 'User-based CF MSE: ' + str(get_mse(user_prediction, test))
+print 'Item-based CF MSE: ' + str(get_mse(item_prediction, test))
+
+
+
+
+
+
